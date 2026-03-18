a27b7e637b772d75fabcd9765c533bac93cac277
Lzzzii <lzzzii10@163.com>
2026-03-18 18:02:47 +0800

    优化部署配置

--- libs/cli/textual_run.py
+++ libs/cli/textual_run.py
@@ -1,4 +1,13 @@
+import os
+
 from textual_serve.server import Server
 
-server = Server("python -m deepagents_cli.textual_web", title="民心智能体")
+public_url = os.environ.get("TEXTUAL_PUBLIC_URL", "http://localhost:8000")
+server = Server(
+    "python -m deepagents_cli.textual_web",
+    host="0.0.0.0",
+    port=8000,
+    public_url=public_url,
+    title="民心智能体",
+)
 server.serve()
